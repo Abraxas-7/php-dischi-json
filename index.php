c6bd218dd509afc3c36c79d790647e7c2b371a13
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-03-19 14:59:34 +0100

    Add initial music catalog and display page

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+$json_music = file_get_contents('./music.json');
+$music = json_decode($json_music, true);
+
+// var_dump($music);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <h1 class="text-center py-3">Dischi musicali</h1>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row g-3">
+
+                <?php
+                foreach ($music as $disco) {
+                    echo '
+                        <div class="col-3">
+                            <div class="card">
+                                <img src="' . $disco["url_cover"] . '" class="card-img-top" alt="' . $disco["titolo"] . '">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center mb-3">' . $disco["titolo"] . '</h5>
+                                    <h6 class="card-subtitle text-center text-muted mb-3"> ' . $disco["artista"] . '</h6>
+                                    <p class="card-text text-center">' . $disco["anno_pubblicazione"] . '</p>
+                                    <p class="card-text text-center">' . $disco["genere"] . '</p>
+                                </div>
+                            </div>
+                        </div>
+                    ';
+                }
+                ?>
+
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
